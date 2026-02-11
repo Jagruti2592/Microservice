598ad1cc221c63a5a9a06a70e74a0eb97e6ccc44
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2026-02-11 14:01:31 -0500

    Added Play.Common nuget packgae to project

---
+++ src/Play.Catalog.Service/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+
+
+using System;
+
+namespace Play.Catalog.Service.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; init; }
+
+    }
+}
--- src/Play.Catalog.Service/Entities/Item.cs
+++ src/Play.Catalog.Service/Entities/Item.cs
@@ -4,7 +4,8 @@ using System;
 
 namespace Play.Catalog.Service.Entities
 {
-    public record Item
+
+    public record Item : IEntity
     {
         public Guid Id { get; init; }
         public string Name { get; init; }
